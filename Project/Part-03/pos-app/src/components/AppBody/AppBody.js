a0df1328c30cd177239f2c7415843ba8a0b1e6ea
Asif Mahmud <asifmahmud3472@gmail.com>
2022-12-09 19:39:09 -0500

    pagination done

--- Project/Part-03/pos-app/src/components/AppBody/AppBody.js
+++ Project/Part-03/pos-app/src/components/AppBody/AppBody.js
@@ -5,19 +5,13 @@ import "./AppBody.css";
 
 const AppBody = ({products}) => {
     const itemsPerPage = 10
-    // Here we use item offsets; we could also use page offsets
-    // following the API or data you're working with.
     const [itemOffset, setItemOffset] = useState(0);
 
-    // Simulate fetching items from another resources.
-    // (This could be items from props; or items loaded in a local state
-    // from an API endpoint with useEffect and useState)
     const endOffset = itemOffset + itemsPerPage;
     console.log(`Loading items from ${itemOffset} to ${endOffset}`);
     const currentItems = products.slice(itemOffset, endOffset);
     const pageCount = Math.ceil(products.length / itemsPerPage);
 
-    // Invoke when user click to request another page.
     const handlePageClick = (event) => {
         const newOffset = (event.selected * itemsPerPage) % products.length;
         console.log(
